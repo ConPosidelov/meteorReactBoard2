d9d1d79204563310955421498a98f73df8446e42
dead-drunk <posideloff@gmail.com>
2017-05-04 12:40:55 +0300

    0405-2

--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -1,6 +1,4 @@
-import '../../api/documents/methods.js';
-import '../../api/documents/server/publications.js';
-
+//
 import '../../api/users/server/methods.js';
 import '../../api/users/server/publications.js';
 
