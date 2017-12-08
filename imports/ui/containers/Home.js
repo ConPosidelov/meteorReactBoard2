fd8a51b6b6d7516b279c6f64e389525a2a5faf3a
dead-drunk <posideloff@gmail.com>
2017-12-08 21:40:12 +0200

    Update Home.js

--- imports/ui/containers/Home.js
+++ imports/ui/containers/Home.js
@@ -1,12 +1,6 @@
 import { composeWithTracker } from 'react-komposer';
 import { Meteor } from 'meteor/meteor';
 import Home from '../components/Home.jsx';
-let data = {
-    user: 'cow',
-    pass: '12345'
-}
-//const composer = (props, onData) => onData(null, { data });
-
 
 const composer = (props, onData) => onData(null, { hasUser: Meteor.user() });
 export default composeWithTracker(composer, {}, {}, { pure: false })(Home);
