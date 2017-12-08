c3d783c1536ac36b7780bb034b094711602d5605
dead-drunk <posideloff@gmail.com>
2017-12-08 21:02:11 +0200

    Update MyBidsContact.jsx

--- imports/ui/components/dashboard/contacts/MyBidsContact.jsx
+++ imports/ui/components/dashboard/contacts/MyBidsContact.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import {Row, Col,  Panel} from 'react-bootstrap';
 
-// 3 строки обязательно для матириал УИ
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-//import injectTapEventPlugin from 'react-tap-event-plugin';
-//injectTapEventPlugin ();
-
-
 
 import {List, ListItem} from 'material-ui/List';
 import ActionGrade from 'material-ui/svg-icons/action/grade';
@@ -23,9 +18,7 @@ const IMGPATH = cv.IMGPATH;
 class MyBidsContact extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-
-        };
+        this.state = {};
         this.delBid = this.delBid.bind(this);
     }
 
@@ -33,16 +26,12 @@ class MyBidsContact extends React.Component {
         console.log("id", id);
         Meteor.call('contact.bid.del', id,  (err, res) => {
             console.log('delBid=res', res);
-
         });
-
     }
 
 
     render() {
         const {dataList} = this.props;
-
-        //console.log("dataList", dataList);
 
         let showList = dataList.map((item, key) => {
             return (
@@ -51,7 +40,6 @@ class MyBidsContact extends React.Component {
                   primaryText={item.fullName +' ( '+item.nicName+' )'}
                   rightIcon={<ActionDelete color={pinkA200} onClick= {()=> this.delBid(item._id)}/>}
                   leftAvatar={<Avatar src={IMGPATH + item.avatarSrc}  size={40}/>}
-
                 />
                 <Divider inset={true} />
             </div>
@@ -62,9 +50,7 @@ class MyBidsContact extends React.Component {
             <MuiThemeProvider>
                 <Panel header="Bids" bsStyle="info">
                     <List>
-
                         {showList}
-
                     </List>
                 </Panel>
             </MuiThemeProvider>
@@ -72,7 +58,6 @@ class MyBidsContact extends React.Component {
       );
     }
 }
-
 
 
 export default MyBidsContact;
