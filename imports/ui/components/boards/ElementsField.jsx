936e3612cc3e3897fd63f62eeeed1c6c5fa32820
dead-drunk <posideloff@gmail.com>
2017-12-08 21:25:27 +0200

    Update ElementsField.jsx

--- imports/ui/components/boards/ElementsField.jsx
+++ imports/ui/components/boards/ElementsField.jsx
@@ -7,40 +7,20 @@ import Boards from '../../../api/dashboard/boards/collections.js';
 
 
 class ElementsField extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            
-        };
-    }
- 
+     
   
     shouldComponentUpdate(nextProps, nextState) {
-       //console.log('ElementsFieldShould=', this.props);
-        
         return true
     }
 
-    componentDidMount() {
-       
-          
-    }
-
     render() {
-       console.log('ElementsField=', this.props);
-     
         const {body} = this.props;
-        
-
         const elementsList = body.map((item, index) => {
             
         });
-
-        
+       
         return (
             <div className='cursorSynchro elementsField'>
-            gggggggg
-              
                 {!body.length ? elementsList : ''}
             </div>
         );
