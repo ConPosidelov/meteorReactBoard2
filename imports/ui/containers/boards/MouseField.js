9bc27c3932c0a5198ef3d6f5058bf854aac7521e
dead-drunk <posideloff@gmail.com>
2018-04-19 22:53:22 +0300

    Update MouseField.js

--- imports/ui/containers/boards/MouseField.js
+++ imports/ui/containers/boards/MouseField.js
@@ -1,36 +1,37 @@
-import React ,{ Component }from 'react';
-import { composeWithTracker } from 'react-komposer';
-import { Meteor } from 'meteor/meteor';
-import Loading from '../../components/Loading.js';
+import React, { Component } from "react";
+import { composeWithTracker } from "react-komposer";
+import { Meteor } from "meteor/meteor";
+import Loading from "../../components/Loading.js";
 
 //import MouseField  from '../../components/boards/MouseField.jsx';
-import MouseField  from '../../components/boards/MouseField_New1.jsx';
-import Boards from '../../../api/dashboard/boards/collections.js';
+import MouseField from "../../components/boards/MouseField_New1.jsx";
+import Boards from "../../../api/dashboard/boards/collections.js";
 
+const composer = ({ boards }, onData) => {
+  const subscription = Meteor.subscribe("boards.members", boards);
 
-const composer = ({boards}, onData) => {
-    const subscription = Meteor.subscribe('boards.members', boards);
-  
-    if (subscription.ready()) {
-        let doc = Boards.find({_id: boards}, {fields: {members: 1}}).fetch()[0];
-        let {members} = doc;
-        let newMembers = members.map(item => {
-            if(item.id !== Meteor.userId()){
-                return {
-                    avatarSrc: item.avatarSrc,
-                    active: item.active,
-                    nicName: item.nicName,
-                    color: item.color,
-                    cursorX: item.cursorX,
-                    cursorY: item.cursorY
-               }
-            } else {
-               return {} 
-            }
-
-        }); 
-        onData(null, {});
-    }
+  if (subscription.ready()) {
+    let doc = Boards.find(
+      { _id: boards },
+      { fields: { members: 1 } }
+    ).fetch()[0];
+    let { members } = doc;
+    let newMembers = members.map(item => {
+      if (item.id !== Meteor.userId()) {
+        return {
+          avatarSrc: item.avatarSrc,
+          active: item.active,
+          nicName: item.nicName,
+          color: item.color,
+          cursorX: item.cursorX,
+          cursorY: item.cursorY
+        };
+      } else {
+        return {};
+      }
+    });
+    onData(null, {});
+  }
 };
 
 const WithTracker = composeWithTracker(composer, Loading)(MouseField);
