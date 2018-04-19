8283c7236427b95ca29f52ab0f781bc9205ad5f1
dead-drunk <posideloff@gmail.com>
2018-04-19 22:26:44 +0300

    Update TopMenu.jsx

--- imports/ui/components/boards/TopMenu.jsx
+++ imports/ui/components/boards/TopMenu.jsx
@@ -1,78 +1,74 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {Row, Col, Glyphicon, Nav, NavItem } from 'react-bootstrap';
-import { Grid, Button, Icon, Menu, Segment, Divider, List} from 'semantic-ui-react';
-import handleLogout from '../../../modules/common/handleLogout.js';
-import STORE from '../../states/boards.js';
-import AvatarInCircle from '../common/user/avatarInCircle.jsx';
-import MembersList from './MembersList.jsx';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Glyphicon, Nav, NavItem } from "react-bootstrap";
+import {
+  Grid,
+  Button,
+  Icon,
+  Menu,
+  Segment,
+  Divider,
+  List
+} from "semantic-ui-react";
+import handleLogout from "../../../modules/common/handleLogout.js";
+import STORE from "../../states/boards.js";
+import AvatarInCircle from "../common/user/avatarInCircle.jsx";
+import MembersList from "./MembersList.jsx";
 
 class TopMenu extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          activeItem: '',
-          topMenuStyle: {
-            height: '60px'
-          }
-        };
-    }
-    shouldComponentUpdate(nextProps, nextState) {
-      const {data} = this.props;
-      //const {members} = data;
-      const {activeItem, topMenuStyle} = this.state;
-      if(activeItem !== nextState.activeItem) return true;
-      if(topMenuStyle !== nextState.topMenuStyle) return true;      
-      return false
-    }
-    componentDidMount() {
-      const {boards} = this.props.match.params;
-      const {activeItem} = this.state;
-      STORE.setTopMenu(boards, activeItem); 
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: "",
+      topMenuStyle: {
+        height: "60px"
+      }
+    };
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    const { data } = this.props;
+    //const {members} = data;
+    const { activeItem, topMenuStyle } = this.state;
+    if (activeItem !== nextState.activeItem) return true;
+    if (topMenuStyle !== nextState.topMenuStyle) return true;
+    return false;
+  }
+  componentDidMount() {
+    const { boards } = this.props.match.params;
+    const { activeItem } = this.state;
+    STORE.setTopMenu(boards, activeItem);
+  }
 
-    handleItemClick= (e, { name }) =>{
-      this.setState({ activeItem: name });
-      const {boards} = this.props.match.params;
-      STORE.setTopMenu(boards, name); 
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name });
+    const { boards } = this.props.match.params;
+    STORE.setTopMenu(boards, name);
+  };
+  handleShare = () => {};
 
-    }  
-    handleShare = ()=> {
+  render() {
+    console.log("TopMenu=", this.props);
 
-    }
+    const { activeItem, pushSidebar, topMenuStyle } = this.state;
+    const { history, data } = this.props;
+    const { nicName, boardName } = data;
+    const { match } = this.props;
+    const { params } = match;
 
-    render() {
-        console.log('TopMenu=', this.props);
-
-        const {activeItem, pushSidebar, topMenuStyle} = this.state;
-        const {history, data} = this.props;
-        const {nicName, boardName} = data;
-        const {match} = this.props;
-        const {params} = match;
-
-
-
-        return (
-
-            <div className="topMenu">
-
-              <Link to="/dashboard"  className="toDashboard">
-                <div className="sidebar_brand-logo material-icons">casino</div>
-
-              </Link>
-              <div className="boardName">
-                 {boardName}
-              </div>
-              <div className="topMenuLeftTools">
-                <div className="toolsItem">File</div>
-                <div className="toolsItem">Edit</div>
-                <div className="toolsItem">View</div>
-                <div className="toolsItem">Settings</div>
-                <div className="toolsItem">Help</div>
-              </div>
-              {/*
+    return (
+      <div className="topMenu">
+        <Link to="/dashboard" className="toDashboard">
+          <div className="sidebar_brand-logo material-icons">casino</div>
+        </Link>
+        <div className="boardName">{boardName}</div>
+        <div className="topMenuLeftTools">
+          <div className="toolsItem">File</div>
+          <div className="toolsItem">Edit</div>
+          <div className="toolsItem">View</div>
+          <div className="toolsItem">Settings</div>
+          <div className="toolsItem">Help</div>
+        </div>
+        {/*
               <List horizontal relaxed className="topMenuLeftTools">
                
                 <List.Item>
@@ -98,24 +94,28 @@ class TopMenu extends React.Component {
                 
               </List>
               */}
-              <div className="userContainer" style = {topMenuStyle}>
-                <div className="userNav">
-                  <div className="userNic" title="user Nic">{nicName}</div>
-                  <div className="logOut" title="Logout" onClick={()=> handleLogout(history)}>
-                        <Glyphicon glyph="log-out" />
-                    </div>
-                </div>
-                <div className="userNavTwo">
-                  
-                  <div className="share" title="Share" onClick={this.handleShare}>
-                   <Button primary>Share</Button> 
-                  </div>
+        <div className="userContainer" style={topMenuStyle}>
+          <div className="userNav">
+            <div className="userNic" title="user Nic">
+              {nicName}
+            </div>
+            <div
+              className="logOut"
+              title="Logout"
+              onClick={() => handleLogout(history)}
+            >
+              <Glyphicon glyph="log-out" />
+            </div>
+          </div>
+          <div className="userNavTwo">
+            <div className="share" title="Share" onClick={this.handleShare}>
+              <Button primary>Share</Button>
+            </div>
+          </div>
+        </div>
 
-                </div>
-              </div>
-
-              <List horizontal  floated='right'  className="topMenuRightTools">
-              {/*
+        <List horizontal floated="right" className="topMenuRightTools">
+          {/*
                 <List.Item >
                   <List.Content floated='left'>
                     <Button icon basic
@@ -129,17 +129,11 @@ class TopMenu extends React.Component {
                   </List.Content>
                 </List.Item>
               */}
-                <MembersList params = {params}/>
-
-
-              </List>
-
-
-            </div>
-        );
-    }
-
-};
-
+          <MembersList params={params} />
+        </List>
+      </div>
+    );
+  }
+}
 
 export default TopMenu;
