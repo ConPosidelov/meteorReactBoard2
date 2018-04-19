207e342bfa41669d5ee4e4b1bc932e1dd41a4551
dead-drunk <posideloff@gmail.com>
2018-04-19 21:38:30 +0300

    Update methods.js

--- imports/api/users/server/methods.js
+++ imports/api/users/server/methods.js
@@ -9,17 +9,12 @@ Meteor.methods({
         check(doc, Schema.UserProfileExt1);
 
         let oldDoc = Meteor.users.find({_id: this.userId }).fetch();
-        // проверяем существование поля nicName
         if((!oldDoc[0].profileExt || !oldDoc[0].profileExt.nicName) && !doc.nicName) return 'notExist';
 
-
-        //debugger;
-        //http://192.168.1.101:8080/?port=5858
         let newDoc = {...oldDoc[0].profileExt, ...doc};
         Meteor.users.update(this.userId, {
           $set: {
             profileExt: newDoc
-
           }
         });
     },
@@ -44,12 +39,9 @@ Meteor.methods({
 
     'users.profileExt1.check'(val) {
         check(val, String);
-        let res = Meteor.users.find({'profileExt.nicName': val }).fetch();
+        let res = Meteor.users.find({ 'profileExt.nicName': val }).fetch();
 
         if(res && res.length > 0) return 'notUnique';
         return 'unique';
     },
-
-
-
 });
