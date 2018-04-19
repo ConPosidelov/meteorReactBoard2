1c9458282a2f28bac841bc6a887826cf16e96d8b
dead-drunk <posideloff@gmail.com>
2018-04-19 21:52:37 +0300

    Update routes.js

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,10 +1,7 @@
-/* eslint-disable max-len */
-
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-//import {history} from './myHistory';
 
 import Home from '../../ui/containers/Home.js';
 import Dashboard from '../../ui/containers/Dashboard.js';
@@ -22,28 +19,20 @@ import ResetPassword from '../../ui/pages/ResetPassword.js';
 import Signup from '../../ui/pages/Signup.js';
 
 
-
 const isLoggedIn = () => !!Meteor.userId();
-
 
 Meteor.startup(() => {
   render(
     <Router>
-      
       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/dashboard' render={(props)=>(isLoggedIn() ? <Dashboard {...props}/> : <Redirect to="/login"/>)}/>
-        
-        <Route path="/boards/:boards" render={(props)=>(isLoggedIn() ? <Boards {...props}/> : <Redirect to="/login"/>)}/>
-         
-
-        
-        <Route path="/login" component={ Login } />
-        <Route path="/recover-password" component={ RecoverPassword } />
-        <Route path="/reset-password/:token" component={ ResetPassword } />
-        <Route path="/signup" component={ Signup } />
-      
-        <Route component={ NotFound } />
+        <Route exact path="/" component={Home}/>
+        <Route path="/dashboard" render={ (props) => (isLoggedIn() ? <Dashboard {...props}/> : <Redirect to="/login"/>) }/>
+        <Route path="/boards/:boards" render={ (props) => (isLoggedIn() ? <Boards {...props}/> : <Redirect to="/login"/>) }/>
+        <Route path="/login" component={Login} />
+        <Route path="/recover-password" component={RecoverPassword}/>
+        <Route path="/reset-password/:token" component={ResetPassword}/>
+        <Route path="/signup" component={Signup}/>
+        <Route component={NotFound}/>
       </Switch>
     </Router>,
     document.getElementById('react-root')
