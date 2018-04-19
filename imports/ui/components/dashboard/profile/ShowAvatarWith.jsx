e18294e737c8824294531ab6dab67317c788b2fb
dead-drunk <posideloff@gmail.com>
2018-04-19 22:42:21 +0300

    Update ShowAvatarWith.jsx

--- imports/ui/components/dashboard/profile/ShowAvatarWith.jsx
+++ imports/ui/components/dashboard/profile/ShowAvatarWith.jsx
@@ -1,53 +1,80 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
+import React from "react";
+import { Meteor } from "meteor/meteor";
 
+const ShowAvatarWith = props => {
+  const saveValue = value => {
+    Meteor.call("users.profileExt2.update", value);
+  };
 
-const ShowAvatarWith = (props) => {
+  return (
+    <form role="form" className="formUser profileExt2">
+      <div
+        className="form-group radio showAvatarWith"
+        onChange={e => {
+          let value = e.target.value;
+          saveValue(value);
+          props.showAvatarSet(value);
+        }}
+      >
+        <label className="control-label">
+          <input
+            type="radio"
+            name="showAvatar"
+            className="optionShowAvatar"
+            value="Facebook"
+          />
+          <p>Facebook</p>
+        </label>
+        <label className="control-label">
+          <input
+            type="radio"
+            name="showAvatar"
+            className="optionShowAvatar"
+            value="Google"
+          />
+          <p>Google</p>
+        </label>
+        <label className="control-label">
+          <input
+            type="radio"
+            name="showAvatar"
+            className="optionShowAvatar"
+            value="Twitter"
+          />
+          <p>Twitter</p>
+        </label>
+        <label className="control-label">
+          <input
+            type="radio"
+            name="showAvatar"
+            className="optionShowAvatar"
+            value="GitHub"
+          />
+          <p>GitHub</p>
+        </label>
+        <label className="control-label">
+          <input
+            type="radio"
+            name="showAvatar"
+            className="optionShowAvatar"
+            value="LinkedIn"
+          />
+          <p>LinkedIn</p>
+        </label>
 
-    const saveValue = (value) => {
-        Meteor.call('users.profileExt2.update', value);
-    }
-
-    return (
-
-        <form role="form" className="formUser profileExt2">
-            <div className="form-group radio showAvatarWith"  onChange={
-                (e) => {
-                    let value = e.target.value;
-                    saveValue(value);
-                    props.showAvatarSet(value);
-                }
-            }>
-                <label className="control-label">
-                    <input type="radio" name="showAvatar" className="optionShowAvatar" value="Facebook"/>
-                    <p>Facebook</p>
-                </label>
-                <label className="control-label">
-                    <input type="radio" name="showAvatar" className="optionShowAvatar" value="Google"/>
-                    <p>Google</p>
-                </label>
-                <label className="control-label">
-                    <input type="radio" name="showAvatar" className="optionShowAvatar" value="Twitter"/>
-                    <p>Twitter</p>
-                </label>
-                <label className="control-label">
-                    <input type="radio" name="showAvatar" className="optionShowAvatar" value="GitHub"/>
-                    <p>GitHub</p>
-                </label>
-                <label className="control-label">
-                    <input type="radio" name="showAvatar" className="optionShowAvatar" value="LinkedIn"/>
-                    <p>LinkedIn</p>
-                </label>
-
-                <label className="control-label">
-                    <input type="radio" name="showAvatar" className="optionShowAvatar" value="own"/>
-                    <p>own</p>
-                </label>
-            </div>
-        </form>
-
-
-    );
+        <label className="control-label">
+          <input
+            type="radio"
+            name="showAvatar"
+            className="optionShowAvatar"
+            value="own"
+          />
+          <p>own</p>
+        </label>
+      </div>
+    </form>
+  );
 };
 
 export default ShowAvatarWith;
+
