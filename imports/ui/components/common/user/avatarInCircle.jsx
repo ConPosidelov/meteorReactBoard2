89b80a57d14223e92e28beba3595b90ffb88f29d
dead-drunk <posideloff@gmail.com>
2018-04-19 22:30:00 +0300

    Update avatarInCircle.jsx

--- imports/ui/components/common/user/avatarInCircle.jsx
+++ imports/ui/components/common/user/avatarInCircle.jsx
@@ -1,45 +1,44 @@
-import React from 'react';
+import React from "react";
 const IMGPATH = cv.IMGPATH;
 
+const avatarInCircle = props => {
+  //console.log('avatarInCircle', props);
+  let { avatarSrc, size, color, float, border } = props;
+  let content = "";
+  if (!color) color = "orange";
+  if (!size) size = "40";
 
+  let style = {
+    border: `solid ${size / 10}px ${color}`,
+    width: `${size}px`,
+    height: `${size}px`,
+    fontSize: `${size * 0.7}px`,
+    paddingTop: `${size * 0.35}px`
+  };
+  if (border) style.border = border;
 
-const avatarInCircle = (props) => {
-    //console.log('avatarInCircle', props);
-    let {avatarSrc, size, color, float, border} = props;
-    let content = '';
-    if(!color) color = 'orange';
-    if(!size) size = '40';
+  if (!avatarSrc) {
+    let h = Math.floor(Math.random() * 360);
+    let bgColor = `hsl(${h}, 80%, 80%)`;
+    //style.backgroundColor = bgColor;
+    let abc = "qwertyuiopasdfghjklzxcvbnm";
+    let abcLength = abc.length;
+    content = abc[Math.floor(Math.random() * abcLength)];
+  } else {
+    style.background = `url('${IMGPATH +
+      avatarSrc}') transparent center center no-repeat`;
+    style.backgroundSize = "cover";
+  }
 
-    let style = {
-        border: `solid ${size/10}px ${color}`,
-        width : `${size}px`,
-        height : `${size}px`,
-        fontSize: `${size * .7}px`,
-        paddingTop: `${size * .35}px`
-    };
-    if(border) style.border = border;
+  if (float) {
+    style.float = float;
+  }
 
-    if(!avatarSrc) {
-        let h = Math.floor(Math.random() * 360);
-        let bgColor = `hsl(${h}, 80%, 80%)`;
-        //style.backgroundColor = bgColor;
-        let abc = 'qwertyuiopasdfghjklzxcvbnm';
-        let abcLength = abc.length;
-        content = abc[Math.floor(Math.random() * abcLength)];
-    } else {
-        style.background = `url('${IMGPATH + avatarSrc}') transparent center center no-repeat`;
-        style.backgroundSize = 'cover';
-    }
-
-    if(float) {
-        style.float = float;
-    }
-
-    return (
-        <div className="avatarInCircle" style={style}>{content}</div>
-    );
-
-}
-
+  return (
+    <div className="avatarInCircle" style={style}>
+      {content}
+    </div>
+  );
+};
 
 export default avatarInCircle;
