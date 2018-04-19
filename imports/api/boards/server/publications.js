bf87d62ddae7f69feffdaa837cc86329863f7580
dead-drunk <posideloff@gmail.com>
2018-04-19 21:21:19 +0300

    Update publications.js

--- imports/api/boards/server/publications.js
+++ imports/api/boards/server/publications.js
@@ -6,17 +6,17 @@ if (Meteor.isServer) {
     Meteor.publish('boards.for.user', function (boardId) {
         check(boardId, String);
         if (!this.userId) return this.ready();
-        return Boards.find({_id: boardId});
+        return Boards.find({ _id: boardId });
     });
     Meteor.publish('boards.members', function (boardId) {
         check(boardId, String);
         if (!this.userId) return this.ready();
-        return Boards.find({_id: boardId}, {fields: {members: 1, name: 1}});
+        return Boards.find({ _id: boardId }, { fields: { members: 1, name: 1 } });
     });
      Meteor.publish('boards.body', function (boardId) {
         check(boardId, String);
         if (!this.userId) return this.ready();
-        return Boards.find({_id: boardId}, {fields: {body: 1}});
+        return Boards.find({ _id: boardId }, { fields: { body: 1 } });
     });
 
 }
