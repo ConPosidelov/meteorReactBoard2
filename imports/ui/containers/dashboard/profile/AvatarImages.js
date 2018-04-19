4aab392f4a831576a7864481949aa36e0dd1eb9c
dead-drunk <posideloff@gmail.com>
2018-04-19 22:54:28 +0300

    Update AvatarImages.js

--- imports/ui/containers/dashboard/profile/AvatarImages.js
+++ imports/ui/containers/dashboard/profile/AvatarImages.js
@@ -2,7 +2,7 @@ import { composeWithTracker } from 'react-komposer';
 import { Meteor } from 'meteor/meteor';
 import AvatarImages from '../../../components/dashboard/profile/AvatarImages.jsx';
 import Loading from '../../../components/Loading.js';
-import {Images} from '../../../../api/images/collections.js';
+import { Images } from '../../../../api/images/collections.js';
 
 
 
@@ -10,7 +10,7 @@ const composer = ({ params }, onData) => {
   const subscription = Meteor.subscribe('files.images.all');
 
   if (subscription.ready()) {
-   const doc = Images.find({userId: Meteor.userId(), 'meta.type': 'avatar'}, {sort: {'meta.createdAt': -1}}).fetch();
+   const doc = Images.find({ userId: Meteor.userId(), 'meta.type': 'avatar' }, { sort: { 'meta.createdAt': -1 } }).fetch();
     //console.log('avatarImages', doc);
 
     onData(null, { avatarImages: doc });
