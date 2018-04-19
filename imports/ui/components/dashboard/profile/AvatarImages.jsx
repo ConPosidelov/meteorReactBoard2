02b714c1c001966cd73ab14b9170761a2e2d7bbd
dead-drunk <posideloff@gmail.com>
2018-04-19 22:38:20 +0300

    Update AvatarImages.jsx

--- imports/ui/components/dashboard/profile/AvatarImages.jsx
+++ imports/ui/components/dashboard/profile/AvatarImages.jsx
@@ -1,46 +1,53 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Row, Col, Glyphicon, Panel, Button } from 'react-bootstrap';
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { Row, Col, Glyphicon, Panel, Button } from "react-bootstrap";
 
 const IMGPATH = cv.IMGPATH;
 
-
 class AvatarImages extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentUpload: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUpload: false
+    };
+  }
 
-    saveAvatar = (src) =>{
-        Meteor.call('users.profileExt3.update', src);
-    }
+  saveAvatar = src => {
+    Meteor.call("users.profileExt3.update", src);
+  };
 
-    render() {
+  render() {
+    const { avatarImages } = this.props;
 
-        const {avatarImages} = this.props;
+    let showAvatars = avatarImages.map((item, i) => {
+      let imgName = item._id + "." + item.ext;
+      //console.log('imgName', imgName);
+      let avatarSrc = IMGPATH + imgName;
+      //console.log('avatarSrc', avatarSrc);
+      return (
+        <div
+          className="avatarImages"
+          key={"img" + i}
+          onClick={e => {
+            this.saveAvatar(imgName);
+          }}
+        >
+          <img src={avatarSrc} width="100" height="100" alt="avatar" />
+        </div>
+      );
+    });
 
-        let showAvatars = avatarImages.map((item, i) => {
-            let imgName = item._id + '.'+ item.ext;
-            //console.log('imgName', imgName);
-            let avatarSrc = IMGPATH + imgName;
-            //console.log('avatarSrc', avatarSrc);
-            return (
-                <div className="avatarImages" key={'img' + i}
-                    onClick={(e)=>{this.saveAvatar(imgName)}}>
-                    <img src={avatarSrc} width="100" height="100" alt="avatar"/>
-                </div>
-            );
-
-        });
-
-        return (
-            <Panel className='selectAnAvatar' header='Select an avatar' bsStyle="info">
-                {showAvatars}
-            </Panel>
-        );
-    }
+    return (
+      <Panel
+        className="selectAnAvatar"
+        header="Select an avatar"
+        bsStyle="info"
+      >
+        {showAvatars}
+      </Panel>
+    );
+  }
 }
 
 export default AvatarImages;
+
